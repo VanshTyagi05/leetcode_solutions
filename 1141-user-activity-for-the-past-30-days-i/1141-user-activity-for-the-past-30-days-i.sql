8a99d2e6e6a0462967db5ca72e423e4d36ecf5d9
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-03 16:23:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,6 +3,5 @@ SELECT
 activity_date as day,
 count(distinct user_id)  as active_users
 from activity
-where 
-activity_date between date_sub('2019-07-27',interval 29 day) and '2019-07-27'
+WHERE activity_date BETWEEN SUBDATE('2019-07-27', 29) AND '2019-07-27'
 group by day;
