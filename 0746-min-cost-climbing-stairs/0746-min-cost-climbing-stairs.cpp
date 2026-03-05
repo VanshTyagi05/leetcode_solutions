3ccf268f7deacd5cf6af315292a3f09b4ebb14d1
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-05 18:33:03 +0530

    Time: 0 ms (100%), Space: 17.9 MB (6.15%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int fun(int idx,vector<int>&cost){
+    int fun(int idx,vector<int>&cost,vector<int>&dp){
         if(idx>=cost.size()){
             return 0; // we have reached the top
         }
+        if(dp[idx]!=-1)return dp[idx];
         //first cvhoice climb only ne step
-        int onestep=cost[idx]+fun(idx+1,cost);
-        int twostep=cost[idx]+fun(idx+2,cost);
-        return min(onestep,twostep);
+        int onestep=cost[idx]+fun(idx+1,cost,dp);
+        int twostep=cost[idx]+fun(idx+2,cost,dp);
+        return dp[idx]=min(onestep,twostep);
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        
-        int strting0=fun(0,cost);
-        int strting1=fun(1,cost);
+        int n=cost.size();
+        vector<int>dp(n+1,-1);
+        int strting0=fun(0,cost,dp);
+        int strting1=fun(1,cost,dp);
         return min(strting0,strting1);
     }
 };
