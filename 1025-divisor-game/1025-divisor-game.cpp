801a67589d06f6a34bd0ee7225ffeff71b49c704
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-06 17:04:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if(n<=1)return false;
-        for(int x=1;x<n;x++){
-            if(n%x==0){
-                return !divisorGame(n-x);
-            }
-        }
+        if(n%2==0)return true;
         return false;
     }
 };
