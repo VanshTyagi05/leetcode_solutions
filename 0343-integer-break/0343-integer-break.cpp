37456edcac60c7dc1e9e8ea519ce4353615e4356
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-07 16:43:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n==2)return 1;
-        if(n==3)return 2;
-        int threes=n/3;
-        int remainder=n%3;
-        if(remainder==1){
-            threes-=1;
-            remainder=4;
-        }else if(remainder==0){
-            remainder=1;
-        }
+        vector<int> dp(n + 1, 0);
 
-        return (int)(pow(3,threes)*remainder);
+        // Base case: for n=1, no break is possible that results in positive
+        // integers, but we initialize dp[1] = 1 for the multiplication logic to
+        // work.
+        dp[1] = 1;
+
+        // Build the table from 2 up to n
+        for (int i = 2; i <= n; i++) {
+            // Try every possible first cut 'j'
+            for (int j = 1; j < i; j++) {
+                /*
+                   For each cut j, we have two options for the remaining part (i
+                   - j):
+                   1. (i - j): Don't break it further. Product = j * (i - j)
+                   2. dp[i - j]: Use the best product from a previous
+                   subproblem. Product = j * dp[i - j]
+                */
+                int currentMax = max(j * (i - j), j * dp[i - j]);
+
+                // Update dp[i] with the maximum found among all possible cuts
+                dp[i] = max(dp[i], currentMax);
+            }
+        }
+        return dp[n];
     }
 };
