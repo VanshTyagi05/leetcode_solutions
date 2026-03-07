299abe88393b32dc2c19a5067f6957676845577e
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-07 16:52:09 +0530

    Time: 0 ms (100%), Space: 8.8 MB (14.5%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int helper(int n){
-        if(n==1)return 1; // further we cnt break
+    int helper(int n,vector<int>&dp){
+        if(n==1)return 1;
+        if(dp[n]!=-1)return dp[n]; // further we cnt break
         int res=0;
         for(int j=1;j<n;j++){
-          int product=j*max(n-j,helper(n-j));
+          int product=j*max(n-j,helper(n-j,dp));
           res=max(res,product);
         }
-        return res;
+        return dp[n]=res;
     }
     int integerBreak(int n) {
-        return helper(n);
+        vector<int>dp(n+1,-1);
+        return helper(n,dp);
     }
 };
