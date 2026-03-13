2cc03f01e6ec233001a798a8f1635008fb16fb51
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-03-13 22:28:22 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.2%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        int start=0;
+        int end=n-1; 
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=piles[i];
+        }
+        int turn=1;//1->ALice 
+                   //0->Bob
+        int alice=0;//sum for alice
+        int bob=0;//sum for bob
+        while(start<=end){
+            if(piles[start]>=piles[end]){
+              if(turn){
+                alice+=piles[start];
+                start++;
+              }else{
+                bob+=piles[start];
+                start++;
+              }
+            }else{
+                if(turn){
+                alice+=piles[end];
+                end--;
+              }else{
+                bob+=piles[end];
+                end;
+              }
+            }
+
+            // if at any point 
+            if(alice>(sum/2))return true;
+        }
+        return false;
+    }
+};
