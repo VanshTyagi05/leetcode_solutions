007d069245c476ac6b1d4d9e7b7e77ebd3a18ebf
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-04-14 19:46:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        int ans = 0;
         int n = rating.size();
-        int ans=0;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        ans++;
-                    }
+        for (int j = 1; j < n - 1; j++) {
+            int count_left_asc = 0;
+            int count_right_asc = 0;
+            int count_left_dsc = 0;
+            int count_right_dsc = 0;
+            // jth is the middle element
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) {
+                    count_left_asc++;
+                }
+                if (rating[i] > rating[j]) {
+                    count_left_dsc++;
                 }
             }
+            for (int k = j + 1; k < n; k++) {
+                if (rating[j] < rating[k]) {
+                    count_right_asc++;
+                }
+                if (rating[j] > rating[k]) {
+                    count_right_dsc++;
+                }
+            }
+            // Ascending teams: (smaller on left) * (larger on right)
+            ans += (count_left_asc * count_right_asc);
+
+            // Descending teams: (larger on left) * (smaller on right)
+            ans += (count_left_dsc * count_right_dsc);
         }
+
         return ans;
     }
 };
