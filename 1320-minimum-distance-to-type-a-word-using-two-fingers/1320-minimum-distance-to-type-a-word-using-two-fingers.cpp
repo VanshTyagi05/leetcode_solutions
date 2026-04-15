3387e02d4c9c0a83ff60439e5127a9c63db0735a
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-04-15 19:29:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int dp[301][7][7][7][7];
-    pair<int,int> getcoord(char ch){
-        int cur=ch-'A';
-        return {cur/6,cur%6};
-    }
+    int dp[301][26][26];
 
-    int getdist(int x1,int y1,int x2,int y2){
-       return abs(x1-x2)+abs(y1-y2);
+    int getdist(int f1, int f2) {
+        int x1 = f1 / 6;
+        int y1 = f1 % 6;
+        int x2 = f2 / 6;
+        int y2 = f2 % 6;
+        return abs(x1 - x2) + abs(y1 - y2);
     }
-    int solve(string word,int i,int x1,int y1,int x2,int y2){
-        if(i>=word.size()){
+    int solve(string word, int i, int f1, int f2) {
+        if (i >= word.size()) {
             return 0;
         }
-        if(x1>=0 && y1>=0 && x2>=0 && y2>=0 && dp[i][x1][y1][x2][y2]!=-1)return dp[i][x1][y1][x2][y2];
-         char ch=word[i];
-         auto [x,y]=getcoord(ch);
-         int result=INT_MAX;
-        // if non of the finger is used erlier we can use any finger 
-        if(x1==-1 && y1==-1 && x2==-1 && y2==-1){
-            //use finger 1 means finger1 will now moe to x and y
-            return solve(word,i+1,x,y,x2,y2);
+        if (f1<26 && f2<26 && dp[i][f1][f2] != -1)
+            return dp[i][f1][f2];
+        char ch = word[i];
+        int pos = ch - 'A';
+        // if non of the finger is used erlier we can use any finger
+        if (f1 == 26 && f2 == 26) {
+            // use finger 1 means finger1 will now moe to x and y
+            return solve(word, i + 1, pos, f2);
         }
         // if f2 is not used already
-        if(x2==-1 && y2==-1){
-            // use it and dont use f1 as its already used 
-            int movef2=0+solve(word,i+1,x1,y1,x,y);
-            int moveF1=getdist(x1,y1,x,y)+solve(word,i+1,x,y,x2,y2);
-            return min(movef2,moveF1);
+        if (f2 == 26) {
+            // use it and dont use f1 as its already used
+            int movef2 = 0 + solve(word, i + 1, f1, pos);
+            int moveF1 = getdist(f1, pos) + solve(word, i + 1, pos, f2);
+            return min(movef2, moveF1);
         }
 
-        //both areused already 
-        int move1=getdist(x1,y1,x,y)+solve(word,i+1,x,y,x2,y2);
-        int move2=getdist(x2,y2,x,y)+solve(word,i+1,x1,y1,x,y);
+        // both areused already
+        int move1 = getdist(f1, pos) + solve(word, i + 1, pos, f2);
+        int move2 = getdist(f2, pos) + solve(word, i + 1, f1, pos);
 
-        return dp[i][x1][y1][x2][y2]=min(move1,move2);
+        return dp[i][f1][f2] = min(move1, move2);
     }
     int minimumDistance(string word) {
-        memset(dp,-1,sizeof(dp));
-      return solve(word,0,-1,-1,-1,-1);
+        memset(dp, -1, sizeof(dp));
+        return solve(word, 0,26,26);
     }
 };
