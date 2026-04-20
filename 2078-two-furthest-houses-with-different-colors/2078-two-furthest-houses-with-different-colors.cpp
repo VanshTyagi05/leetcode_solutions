ac8f83c571dff095fa92f0d92f5ace6257fdc147
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-04-20 23:06:30 +0530

    Time: 0 ms (100%), Space: 12 MB (72.82%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -2,37 +2,24 @@ class Solution {
 public:
     int maxDistance(vector<int>& colors) {
         int n = colors.size();
-        unordered_map<int, pair<int, int>> mp;
-        for (int i = 0; i < n; i++) {
-            int color = colors[i];
-            if (mp.find(color) == mp.end()) {
-                // first time hai
-                mp[color] = {i, i};
-            } else {
-                // first wale mai minimum daal do second vale aimaxmum
-                mp[color].first = min(mp[color].first, i);
-                mp[color].second = max(mp[color].second, i);
+        int maxDist = 0;
+
+        // Case 1: Distance from the first house (index 0) to the furthest different color
+        for (int i = n - 1; i > 0; i--) {
+            if (colors[i] != colors[0]) {
+                maxDist = max(maxDist, i);
+                break;
             }
         }
 
-        // now we wil itreate on the map and find two things min left most and maximum rightmost from two different  colors
-        int leftmost_color=colors[0];
-        int leftmost_idx=INT_MAX;
-        // finding the minimum idx leftmost_idx for some leftmost_color
-        for(auto const& [key,it]:mp){
-             if(it.first<leftmost_idx){
-                leftmost_idx=it.first;
-                leftmost_color=key;
-             }
-        }
-          int rightmost_idx=INT_MIN;
-        // finding the maximum idx leftmost_idx for some color!=leftmost_color
-        for(auto const& [key,it]:mp){
-             if(it.second>rightmost_idx  && key!=leftmost_color){
-                rightmost_idx=it.second;
-             }
+        // Case 2: Distance from the last house (index n-1) to the furthest different color
+        for (int i = 0; i < n - 1; i++) {
+            if (colors[i] != colors[n - 1]) {
+                maxDist = max(maxDist, (n - 1) - i);
+                break;
+            }
         }
 
-        return abs(rightmost_idx-leftmost_idx);
+        return maxDist;
     }
 };
