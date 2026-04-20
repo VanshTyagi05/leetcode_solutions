1cdeaea4961f90d3b657293014ea8c68df4da758
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-04-20 23:04:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
-        int n=colors.size();
-        int ans=0;
-        for(int i=0;i<n-1;i++){
-            // selecting the ith 
-            int color1=colors[i];
-            for(int j=i+1;j<n;j++){
-                int color2=colors[j];
-                if(color1!=color2){
-                    ans=max(ans,abs(i-j));
-                }
+        int n = colors.size();
+        unordered_map<int, pair<int, int>> mp;
+        for (int i = 0; i < n; i++) {
+            int color = colors[i];
+            if (mp.find(color) == mp.end()) {
+                // first time hai
+                mp[color] = {i, i};
+            } else {
+                // first wale mai minimum daal do second vale aimaxmum
+                mp[color].first = min(mp[color].first, i);
+                mp[color].second = max(mp[color].second, i);
             }
         }
-        return ans;
+
+        // now we wil itreate on the map and find two things min left most and maximum rightmost from two different  colors
+        int leftmost_color=colors[0];
+        int leftmost_idx=INT_MAX;
+        // finding the minimum idx leftmost_idx for some leftmost_color
+        for(auto const& [key,it]:mp){
+             if(it.first<leftmost_idx){
+                leftmost_idx=it.first;
+                leftmost_color=key;
+             }
+        }
+          int rightmost_idx=INT_MIN;
+        // finding the maximum idx leftmost_idx for some color!=leftmost_color
+        for(auto const& [key,it]:mp){
+             if(it.second>rightmost_idx  && key!=leftmost_color){
+                rightmost_idx=it.second;
+             }
+        }
+
+        return abs(rightmost_idx-leftmost_idx);
     }
 };
