52d37c2452a94bda3652cea8489261b253e65760
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-05-14 13:03:53 +0530

    Time: 0 ms (100%), Space: 26.4 MB (73.39%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         int size = nums.size();
-        // A valid base[n] array must have at least 2 elements (n=1 -> [1, 1])
-        if (size < 2) return false; 
-        
+        if(size<2)return false;
         sort(nums.begin(), nums.end());
-        
         for (int i = 0; i < size; i++) {
             if (i == size - 1) {
-                // The last element must mirror the second-to-last element
+                // we are at the last element
                 if (nums[i] != nums[i - 1]) {
                     return false;
                 }
             } else {
-                // Elements before the last one must strictly match 1, 2, 3...
-                if (i + 1 != nums[i]) {
+                if (i + 1 != nums[i])
                     return false;
-                }
             }
         }
         return true;
