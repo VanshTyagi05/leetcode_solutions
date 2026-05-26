d16b5d5e295bf4c22816e17a7abd7576a32305c7
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-05-26 18:02:31 +0530

    Time: 504 ms (9.68%), Space: 53.3 MB (12.19%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)return true;
-        int max_reachable = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            max_reachable = max(max_reachable, i + nums[i]);
-            if(max_reachable>=n-1)return true;
-            if(i==max_reachable && nums[i]==0)return false;
-            
-        }
+   int n;
+   int t[10001];
+   bool recur(int idx,vector<int>&nums){
+    if(idx>=n-1){
+        return t[idx]= true;
+    }
+    if(t[idx]!=-1)return t[idx];
+    for(int i=1;i<=nums[idx];i++){
+        if(recur(idx+i,nums)){
+            return t[idx]= true;
+        };
+    }
 
-        return true;
+    return t[idx]=false;
+   }
+    bool canJump(vector<int>& nums) {
+        n=nums.size();
+        memset(t,-1,sizeof(t));
+        return recur(0,nums);
     }
 };
