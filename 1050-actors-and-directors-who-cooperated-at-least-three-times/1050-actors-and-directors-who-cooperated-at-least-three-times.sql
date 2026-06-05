a057d77190e27c6f76c98203d73b481818a0dd37
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-06-05 23:56:27 +0530

    Time: 385 ms (39.95%), Space: 0B (100%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
-select actor_id,director_id from
-(select actor_id,director_id,count(*) as total_pairs 
+
+select actor_id,director_id
 from ActorDirector
-group by actor_id,director_id) as s
-where total_pairs>=3;
+group by actor_id,director_id
+having count(*) >=3;
+# agr tumne group by function use kiya hai toh count(*) humesha count krta hai created groups ka size
+
