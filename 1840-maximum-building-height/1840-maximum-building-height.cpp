c2367b7a4525d6c99a87403c01caaff12f134b27
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-06-20 14:17:01 +0530

    Time: 63 ms (51.39%), Space: 106.6 MB (84.72%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+
+        restrictions.push_back({1, 0});
+        restrictions.push_back({n, n - 1});
+
+        sort(restrictions.begin(), restrictions.end());
+
+        int m = restrictions.size();
+
+        // Left -> Right
+        for (int i = 1; i < m; i++) {
+            int dist = restrictions[i][0] - restrictions[i - 1][0];
+            restrictions[i][1] =
+                min(restrictions[i][1], restrictions[i - 1][1] + dist);
+        }
+
+        // Right -> Left
+        for (int i = m - 2; i >= 0; i--) {
+            int dist = restrictions[i + 1][0] - restrictions[i][0];
+            restrictions[i][1] =
+                min(restrictions[i][1], restrictions[i + 1][1] + dist);
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i < m; i++) {
+            int d = restrictions[i][0] - restrictions[i - 1][0];
+            int h1 = restrictions[i - 1][1];
+            int h2 = restrictions[i][1];
+
+            ans = max(ans, (d + h1 + h2) / 2);
+        }
+
+        return ans;
+    }
+};
