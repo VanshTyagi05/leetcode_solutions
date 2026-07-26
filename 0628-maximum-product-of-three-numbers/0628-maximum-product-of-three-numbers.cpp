4ccc57b2b0ea9ee13dc179a5f34a6e398ca2d3bb
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-07-26 19:02:28 +0530

    Time: 18 ms (15.31%), Space: 31.7 MB (1.76%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -3,23 +3,12 @@ public:
     int maximumProduct(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        // count the negative numbers
-        int neg=0;
-        for(int i=0;i<3;i++){
-            if(nums[i]<0){
-                neg++;
-            }
-        }
-
-        // now if neg==0
-        if(neg==0){
-            return nums[n-1]*nums[n-2]*nums[n-3];
-        }
-        int num1=nums[n-1]*nums[n-2]*nums[n-3]; // teeno largest
-        int num2=nums[0]*nums[1]*nums[1];
+        
+        int num1=nums[n-1]*nums[n-2]*nums[n-3]; // teeno largest lelo
+        //int num2=nums[0]*nums[1]*nums[2]; // teeno sabse chote dekh lo (2 ngative hue aur 1 positive)
         int num3=nums[0]*nums[1]*nums[n-1]; // sabse chote do ek bada
-        int num4=nums[0]*nums[n-1]*nums[n-2];
+        //int num4=nums[0]*nums[n-1]*nums[n-2]; // ek sabse chota aur do sabse bade
 
-        return max({num1,num2,num3,num4}); 
+        return max({num1,num3}); 
     }
 };
