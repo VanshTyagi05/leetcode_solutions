bfa02d9812ee78041bea68bf3b6833f01b5ce0f3
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-08-06 23:17:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-   int findproduct(int n){
-    int ans=1;
-    while(n>1){
-        ans=ans*(n%10);
-        n/=10;
-    }
-    return ans;
-   }
-    int smallestNumber(int n, int t) {
-        int start=n;
-        int end=100;
-        int ans=1;
-        for(int i=start;i<=end;i++){
-            if(findproduct(i)%t==0)return i;
+    int findProduct(int n) {
+        int ans = 1;
+        while (n > 0) { // Fix: Must check n > 0 to include the last digit
+            ans *= (n % 10);
+            n /= 10;
         }
-        return 0;
+        return ans;
+    }
+
+    int smallestNumber(int n, int t) {
+        // Linearly increment until the condition is met
+        while (true) {
+            if (findProduct(n) % t == 0) {
+                return n;
+            }
+            n++;
+        }
     }
 };
