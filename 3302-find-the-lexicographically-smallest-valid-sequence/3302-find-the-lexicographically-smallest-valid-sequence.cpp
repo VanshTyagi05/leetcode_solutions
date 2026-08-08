d6a7fb2322c35b07d782cda684fe93c3a8f310be
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-08-09 00:32:30 +0530

    Time: 944 ms (5.32%), Space: 438.2 MB (6.38%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,52 +1,64 @@
 class Solution {
+    // memo[i][canChange] stores the maximum index 'j' in word2 we reached from this state.
+    // If we visit this state again with a lower or equal 'j', it means this path is suboptimal.
+    vector<vector<int>> memo;
+    vector<int> result;
+    bool found = false;
+
+    bool solve(int i, int j, int canChange, string& word1, string& word2, vector<int>& current_path) {
+        // Base Case 1: Successfully matched all characters of word2
+        if (j == word2.size()) {
+            result = current_path;
+            found = true;
+            return true;
+        }
+        // Base Case 2: Ran out of characters in word1
+        if (i == word1.size()) {
+            return false;
+        }
+
+        // Memoization check: If we've seen this (i, canChange) state before with a better or equal j, skip it
+        if (memo[i][canChange] >= j) {
+            return false;
+        }
+        memo[i][canChange] = j;
+
+        // Branch 1: Try to INCLUDE word1[i] (Greedy choice for lexicographically smallest index)
+        // Case A: Natural Match
+        if (word1[i] == word2[j]) {
+            current_path.push_back(i);
+            if (solve(i + 1, j + 1, canChange, word1, word2, current_path)) {
+                return true;
+            }
+            current_path.pop_back(); // Backtrack
+        } 
+        // Case B: Mismatch, but we have a modification token left
+        else if (canChange == 1) {
+            current_path.push_back(i);
+            if (solve(i + 1, j + 1, 0, word1, word2, current_path)) {
+                return true;
+            }
+            current_path.pop_back(); // Backtrack
+        }
+
+        // Branch 2: Try SKIPPING word1[i] to pick a later index instead
+        if (solve(i + 1, j, canChange, word1, word2, current_path)) {
+            return true;
+        }
+
+        return false;
+    }
+
 public:
     vector<int> validSequence(string word1, string word2) {
         int n = word1.size();
-        int m = word2.size();
-        vector<int> matched_from_right_side(n, 0);
-        int matched = 0;
-        int i = n - 1;
-        int j = m - 1;
-        while (i >= 0 && j >= 0) {
-            if (word1[i] == word2[j]) {
-                matched++;
-                j--;
-            }
-            matched_from_right_side[i] = matched;
-            i--;
-        }
-        // If there are remaining elements after j exits, fill the rest of the
-        // suffix array
-        while (i >= 0) {
-            matched_from_right_side[i] = matched;
-            i--;
-        }
-        i = 0;
-        j = 0; // restarting from the start to start matching
-        vector<int> ans;
-        bool canChange = true;
-        while (i < n && j < m) {
-            if (word1[i] == word2[j]) {
-                ans.push_back(i);
-                i++;
-                j++;
-                continue;
-            } else {
-                int remaining_matches =
-                    (i + 1 < n) ? matched_from_right_side[i + 1] : 0;
-                if (canChange == true && remaining_matches >= m - j - 1) {
-                    canChange = false;
-                    ans.push_back(i);
-                    i++;
-                    j++;
-                    continue;
-                } else {
-                    // agr aise change na kr pao toh bas i ko aage badha do
-                    i++;
-                }
-            }
-        }
-        if (ans.size() < m) return {};
-        return ans;
+        
+        // Initialize memoization table: size [n][2] initialized to -1
+        memo.assign(n, vector<int>(2, -1));
+        
+        vector<int> current_path;
+        solve(0, 0, 1, word1, word2, current_path);
+        
+        return result;
     }
 };
