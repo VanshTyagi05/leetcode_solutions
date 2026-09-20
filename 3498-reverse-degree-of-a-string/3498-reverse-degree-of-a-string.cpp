30d74eb6006470c8b6e9d28cf24539b7df0bfa67
Vansh Tyagi <159777456+VanshTyagi05@users.noreply.github.com>
2026-09-21 00:41:46 +0530

    Time: 0 ms (100%), Space: 9.5 MB (95.77%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,9 +3,7 @@ public:
     int reverseDegree(string s) {
         int sum=0;
         for(int i=0;i<s.size();i++){
-            char c=s[i];
-            int curr='z'-c+1;
-            sum+=((i+1)*curr); 
+            sum+=((i+1)*('z'-s[i]+1)); 
         }
         return sum;
     }
